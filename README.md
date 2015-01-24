d95a8cb32e9f70a6e7605f382ebbaac177ce5244
4555 <ddorr@msad54.org>
2015-01-24 12:51:44 -0500

    initial commit
    a program promoting our pies

---
+++ README.md
@@ -0,0 +1,3 @@
+# Bread!
+
+program promoting our bread
---
+++ src/prgm/main.java
@@ -0,0 +1,34 @@
+package prgm;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Bread Products");
+		
+		System.out.println("---A List of Bread Products---");
+		
+
+		System.out.println("Seasonal Breads");
+		
+		System.out.println("Ingredients");
+		
+		System.out.println("Corn"
+				+ "Cocoa Powder"
+				+ "Poison Dart Frog Venom"
+				+ "Premium Yeast"
+				+ "Sugar");
+
+
+		System.out.println("contacts");
+		
+		System.out.println("Notes: Other Stuff");
+		
+
+
+		System.out.println("Notes: stuff");
+
+	}
+
+}
